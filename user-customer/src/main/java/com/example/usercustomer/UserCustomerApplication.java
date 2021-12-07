c05f4c042f43df8c423a740749d8aeade257c0f9
LLLLLL <913577706@qq.com>
2021-12-07 10:55:21 +0800

    tijiao

--- user-customer/src/main/java/com/example/usercustomer/UserCustomerApplication.java
+++ user-customer/src/main/java/com/example/usercustomer/UserCustomerApplication.java
@@ -3,9 +3,11 @@ package com.example.usercustomer;
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableDubbo
+@EnableHystrix
 public class UserCustomerApplication {
 
     public static void main(String[] args) {
--- user-customer/src/main/java/com/example/usercustomer/controller/DubboController.java
+++ user-customer/src/main/java/com/example/usercustomer/controller/DubboController.java
@@ -2,17 +2,51 @@ package com.example.usercustomer.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.example.dubbo.userserviceprivoder.bean.UserBean;
+import com.example.dubbo.userserviceprivoder.bean.u1190Kol.U1190KolDBO;
+import com.example.dubbo.userserviceprivoder.bean.u1190Kol.U1190KolMapper;
 import com.example.dubbo.userserviceprivoder.service.UserBeanService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 @RestController
 public class DubboController {
-    @Reference
+    @Reference(check = false,version = "1.0")
     private UserBeanService dubboService;
+    @Reference
+    private U1190KolMapper u1190KolMapper;
 
     @RequestMapping("/findAll")
+    @HystrixCommand(fallbackMethod = "hello")
     public String findAll(){
         return dubboService.doRead(new UserBean(1,20,"张三","浙江省杭州市江干区"));
+    }
+
+    public String hello(){
+
+        return new UserBean(0,0,"游客","空").toString();
+    }
+
+
+    @RequestMapping(value = "/selectKol")
+    public U1190KolDBO selectKol(String puk) {
+        return u1190KolMapper.doRead(puk);
+    }
+
+    @RequestMapping(value = "/getKolList")
+    @HystrixCommand(fallbackMethod = "errorDefaultMethod")
+    public List<U1190KolDBO> getKolList(U1190KolDBO param) {
+
+        return u1190KolMapper.doSelectList(param);
+    }
+
+    public List<U1190KolDBO> errorDefaultMethod(U1190KolDBO param) {
+        return Arrays.asList(new U1190KolDBO("1","游客","男","","","","智慧谷","20","1234567890","","","","","","","","","","","","",""));
     }
 }
