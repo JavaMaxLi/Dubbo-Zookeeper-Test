b14934210ab50205341f2a397c3e14249c087d43
LLLLLL <913577706@qq.com>
2021-12-13 17:12:36 +0800

    netty-dubbo

---
+++ dubbo-netty/src/main/java/dubbonetty/dubbonetty/DubboNettyApplication.java
@@ -0,0 +1,13 @@
+package dubbonetty.dubbonetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboNettyApplication.class, args);
+    }
+
+}
---
+++ dubbo-netty/src/test/java/dubbonetty/dubbonetty/DubboNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package dubbonetty.dubbonetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
